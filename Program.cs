d57e6feb6aa3f526e2502dc38f777b7f7dbd6961
goldeneyezhang <goldeneyezhang@163.com>
2016-12-27 13:24:37 +0800

    m

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.AspNetCore.Builder;
+using System.IO;
 namespace ConsoleApplication
 {
     public class Program
@@ -9,6 +10,8 @@ namespace ConsoleApplication
         {
             new WebHostBuilder().UseKestrel()
            .UseStartup<Startup>()
+           .UseContentRoot(Directory.GetCurrentDirectory())
+           .UseUrls("http://localhost:8888/","http://localhost:9999/")
             .Build().Run();
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,20 @@
-using System;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-
+using Microsoft.Extensions.DependencyInjection;
 namespace ConsoleApplication
 {
     public class Startup
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(context=>context.Response.WriteAsync("Hello World"));
+            //app.Run(context=>context.Response.WriteAsync("Hello World"));
+            app.UseMvc();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
         }
     }
 }
