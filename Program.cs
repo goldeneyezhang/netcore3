d0d8ad0299f54e63d571debd66fc9746ddd99534
goldeneyezhang <goldeneyezhang@163.com>
2016-12-27 11:13:39 +0800

    kestrel

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder().UseKestrel()
+           .UseStartup<Startup>()
+            .Build().Run();
+        }
+    }
+}
